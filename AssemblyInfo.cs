fb2e3a0cc249b1ba6864d37e847c9b73bbbd0ec9
mewonne <mew90@mail.ru>
2015-11-20 23:27:26 +0300

    Added Assembly.cs

---
+++ AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+[assembly:AssemblyCompany("HomeHome")]
+
+[assembly:AssemblyProduct("Monty Hall's Paradox Game")]
+[assembly:AssemblyInformationalVersion("1.1.0.0")]
+[assembly:AssemblyFileVersion("1.1.0.0")]
+[assembly:AssemblyTitle("MontyHall.exe")]
+[assembly:AssemblyDescription("This is Monty Hall's Paradox")]
+
+[assembly:AssemblyVersion("1.1.0.0")]
